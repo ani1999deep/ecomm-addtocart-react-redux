0ece3eedd33fde9c71aeaf49cdd74819b5a71a6f
Anideep1 <anideepbhowmick1@gmail.com>
2024-06-03 16:08:30 +0530

    first commit of ecomm-addtocart-react-redux

---
+++ src/redux/reducers/main.js
@@ -0,0 +1,9 @@
+//All reducers into it
+import { combineReducers } from "redux";
+import { cartreducer } from "./reducer";
+
+const rootred = combineReducers({
+  cartreducer,
+});
+
+export default rootred;
